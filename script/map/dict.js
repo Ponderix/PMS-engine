6d19ab3f6ad63910a4fd21f946c66ad92c051714
haroldTheDeveloper <samuelb0801@gmail.com>
2022-03-06 12:45:46 +0000

    Added Country and type of map to dict.js

--- script/map/dict.js
+++ script/map/dict.js
@@ -2,62 +2,86 @@ const dict = {};
     dict["germany"] = {
         path : "./assets/maps/geojson/Germany_Bundestag-2021.json",
         title : "German Bundestag",
-        copyright : "© Der Bundeswahlleiter, Statistisches Bundesamt, Wiesbaden 2020,<br/>Wahlkreiskarte für die Wahl zum 20. Deutschen Bundestag<br/>Grundlage der Geoinformationen © Geobasis-DE / BKG 2020"
+        copyright : "© Der Bundeswahlleiter, Statistisches Bundesamt, Wiesbaden 2020,<br/>Wahlkreiskarte für die Wahl zum 20. Deutschen Bundestag<br/>Grundlage der Geoinformationen © Geobasis-DE / BKG 2020",
+        country: "Germany",
+        type: "Lower"
     };
     dict["niedersachsen"] = {
         path : "./assets/maps/geojson/Germany_NiedersachsenLandtag-2017.json",
         title : "Niedersachsen Landtag",
-        copyright : "© Landesamt für Statistik Niedersachsen, Hannover 2017<br/>Wahlkreiskarte für die Wahl zum 18. Niedersächsischen Landtag"
+        copyright : "© Landesamt für Statistik Niedersachsen, Hannover 2017<br/>Wahlkreiskarte für die Wahl zum 18. Niedersächsischen Landtag",
+        country: "Germany",
+        type: "State"
     };
     dict["nrw"] = {
         path : "./assets/maps/geojson/Germany_NRWLandtag-2017.json",
         title : "Nordrhein-Westfalen Landtag",
-        copyright : "© Ministerium des Innern des Landes Nordrhein-Westfalen, IT.NRW, Düsseldorf,<br/>Wahlkreiseinteilung des Landes Nordrhein-Westfalen zur Landtagswahl am 14. Mai 2017."
+        copyright : "© Ministerium des Innern des Landes Nordrhein-Westfalen, IT.NRW, Düsseldorf,<br/>Wahlkreiseinteilung des Landes Nordrhein-Westfalen zur Landtagswahl am 14. Mai 2017.",
+        country: "Germany",
+        type: "State"
     };
     dict["englandProposals"] = {
         path : "./assets/maps/geojson/UK_EnglandProposals-2023.json",
         title : "England 2023 Proposals",
-        copyright : "© Crown copyright and database rights<br/>2021 OS 100018289"
+        copyright : "© Crown copyright and database rights<br/>2021 OS 100018289",
+        country: "UK",
+        type: "Hypothetical"
     };
 
     dict["adoria"] = {
         path : "./assets/maps/svg/Adoria_NationalAssembly_HYP.svg",
         title : "Adoria (fictional) National Assembly",
-        copyright : "© Ponderix 2019,<br/>election map for the Adoria political simulation."
+        copyright : "© Ponderix 2019,<br/>election map for the Adoria political simulation.",
+        country: "Hypothetical",
+        type: "Adoria"
     };
     dict["hungary"] = {
         path : "./assets/maps/svg/Hungary_NationalAssembly-2018.svg",
         title : "Hungarian National Assembly",
-        copyright : "© Erinthecute 2021<br/>(CC BY-SA 4.0)"
+        copyright : "© Erinthecute 2021<br/>(CC BY-SA 4.0)",
+        country: "Hungary",
+        type: "Lower"
     };
     dict["germany598"] = {
         path : "./assets/maps/svg/Germany_LargerBundestag_HYP.svg",
         title : "Germany (hypothetical) 598 Seats",
-        copyright : "© Ponderix 2021<br/>(CC BY-SA 4.0)"
+        copyright : "© Ponderix 2021<br/>(CC BY-SA 4.0)",
+        country: "Germany",
+        type: "Hypothetical"
     };
     dict["czechia"] = {
         path : "./assets/maps/geojson/Czechia_Senate-2020.json",
         title : "Czech Republic Senate",
-        copyright : "© Jindra Lacko 2018"
+        copyright : "© Jindra Lacko 2018",
+        country: "Czechia",
+        type: "Senate"
     };
     dict["UK1997"] = {
         path : "./assets/maps/svg/UK_HouseOfCommons-1997.svg",
         title : "UK House of Commons (1997)",
-        copyright : ""
+        copyright : "",
+        country: "UK",
+        type: "Historical"
     };
     dict["2022US"] = {
         path : "./assets/maps/svg/US_HOR-2022.svg",
         title : "United States 2022 HOR Election",
-        copyright : "By Ketrit - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=115019184"
+        copyright : "By Ketrit - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=115019184",
+        country: "US",
+        type: "Lower"
     };
     dict["Ukraine"] = {
         path : "./assets/maps/svg/Ukraine_Rada-2019.svg",
         title : "Ukraine",
-        copyright : "By Tohaomg - Own work (based on data from the website of State voters register), CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=77631482"
+        copyright : "By Tohaomg - Own work (based on data from the website of State voters register), CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=77631482",
+        country: "Ukraine",
+        type: "Lower"
     };
     dict["2012US"] = {
         path : "./assets/maps/svg/US_HOR-2012.svg",
         title : "United States 2012 HOR Election",
-        copyright : "By Mr. Matté - File:113th U.S. Congress House districts alternate.svg, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=22588251"
+        copyright : "By Mr. Matté - File:113th U.S. Congress House districts alternate.svg, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=22588251",
+        country: "US",
+        type: "Historical"
     };
 export {dict};
